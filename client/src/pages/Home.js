44d26e8a5c4e720942ab54e9fc36e375936aea41
Marshall Baccari <mbaccari@uw.edu>
2022-09-14 22:18:52 -0700

    Signup added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { useNavigate, Link } from "react-router-dom";
+import { useCookies } from 'react-cookie';
 
 import Auth from '../utils/Auth'
 
 const Home = () => {
+    const [cookies, setCookie, removeCookie] = useCookies(['token']);
+
+    const loggedIn = () => {
+        if(!cookies.token) {
+            console.log('no token')
+            return false;
+        } else if(Auth.isTokenExpired(cookies.token)) {
+            console.log('expired token')
+            removeCookie('token',{path:'/'});
+            return false;
+        } else if(Auth.isToken(cookies.token)) {
+            console.log('token plus not expired makes me happy boi')
+            return true;
+        }
+    }
+    
+
     return (
         <main>
-            {Auth.loggedIn() ? `logged in` : `not logged in`}
+            {!loggedIn() ? `not logged in` : `logged in`}
         </main>
     )
 }
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -5,8 +5,8 @@ import axios from 'axios';
 
 import Auth from '../utils/Auth';
 
-const Login = () => {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+const Signup = () => {
+  const [formState, setFormState] = useState({username:'', email: '', password: '' });
 
   const [cookies, setCookie, removeCookie] = useCookies(['token'])
 
@@ -37,15 +37,10 @@ const Login = () => {
   // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
-    const { email, password } = formState;
     axios({
       method: 'post',
-      url: 'http://localhost:3080/api/users/login',
-      data: {
-        email: email,
-        password: password
-      }
+      url: 'http://localhost:3080/api/users/signup',
+      data: formState
     }).then(res => {
       setCookie('token', res.data)
     })
@@ -57,26 +52,38 @@ const Login = () => {
     // });
   };
   return (
+    <>
     <main className="flex-row justify-center mb-4 container">
       <div className="row">
 
         <div className="col-md-2 col-xs-0"></div>
 
         <div className="col-md-8 col-xs-12">
-          <h4 className="text-dark p-2 text-center display-4 fw-bold">Login</h4>
+          <h4 className="text-dark p-2 text-center display-4 fw-bold">Become a Trainer!</h4>
           <div className="card-body">
-
-          {loggedIn() ? (
-            <p>
-              Success! You may now head{' '}
-              <Link to="/">back to the homepage.</Link>
-            </p>
-          ) : (
-          <form onSubmit={handleFormSubmit}>
+            {loggedIn() ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+              
+              <form onSubmit={handleFormSubmit}>
                 <div className="form-outline mb-4">
-                  <label className="form-label fs-5" htmlFor="form3Example1cg">Email:</label>
+                  <label className="form-label fs-5" htmlFor="form3Example1cg">UserName:</label>
                   <input 
                     type="text"
+                    name="username"
+                    className="form-control form-control-lg shadow-sm" 
+                    value={formState.username}
+                    onChange={handleChange}
+                  />
+                  
+                </div>
+                <div className="form-outline mb-4">
+                  <label className="form-label fs-5" htmlFor="form3Example3cg">Email:</label>
+                  <input 
+                    type="email" 
                     name="email"
                     className="form-control form-control-lg shadow-sm" 
                     value={formState.email}
@@ -100,59 +107,12 @@ const Login = () => {
                   className="btn btn-block btn-dark bg-danger mt-2 shadow"
                   style={{ cursor: 'pointer' }}
                   type="submit"
-                  >Login</button>
+                  >Start Training!</button>
                 </div>
-                <p className="text-center text-muted mt-5 mb-0">Don't have an account? <Link to="/signup" className="fw-bold text-body">Register Here</Link></p>
-              </form>
-          )}
-              
-            {/* {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-              
-              <form onSubmit={handleFormSubmit}>
-                <div className="form-outline mb-4">
-                  <label className="form-label fs-5" htmlFor="form3Example1cg">Email:</label>
-                  <input 
-                    type="text"
-                    name="email"
-                    className="form-control form-control-lg shadow-sm" 
-                    value={formState.name}
-                    onChange={handleChange}
-                  />
-                  
-                </div>
-                <div className="form-outline mb-4">
-                  <label className="form-label fs-5" htmlFor="form3Example4cg">Password:</label>
-                  <input 
-                    type="password"
-                    name="password"
-                    className="form-control form-control-lg shadow-sm" 
-                    value={formState.password}
-                    onChange={handleChange}
-                  />
-                  
-                </div>
-                <div className="d-flex justify-content-center">
-                  <button
-                  className="btn btn-block btn-dark bg-danger mt-2 shadow"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                  >Login</button>
-                </div>
-                <p className="text-center text-muted mt-5 mb-0">Don't have an account? <Link to="/signup" className="fw-bold text-body">Register Here</Link></p>
+                <p className="text-center text-muted mt-5 mb-0">Have already an account? <Link to="/login" className="fw-bold text-body">Login Here</Link></p>
               </form>
 
-            )} */}
-
-            {/* {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )} */}
+            )}
           </div>
         </div>
 
@@ -160,7 +120,7 @@ const Login = () => {
 
       </div>
     </main>
+    </>
   );
 };
-
-export default Login;
+export default Signup;
