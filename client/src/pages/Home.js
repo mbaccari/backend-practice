8a7e3728f0f07a1a01415de342d71f6f445e7af6
Marshall Baccari <mbaccari@uw.edu>
2022-11-15 20:02:07 -0800

    reformatted slightly. removed console.logs and unused imports

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate, Link } from "react-router-dom";
 import { useCookies } from 'react-cookie';
-import decode from 'jwt-decode'
 
 import styles from './Home.module.css'
 
-import Api from '../utils/Api'
 import Auth from '../utils/Auth'
-import UserCard from '../components/UserCard';
-import PostForm from '../components/PostForm';
 import axios from 'axios';
 import Nav from '../components/Nav';
 import PostCard from '../components/PostCard';
 
 const Home = () => {
-    const [cookies, setCookie, removeCookie] = useCookies(['token']);
+    const [cookies, removeCookie] = useCookies(['token']);
     const [decodedToken, setDecodedToken] = useState('');
     const [posts, setPosts] = useState(null)
     useEffect(() => {
@@ -24,18 +19,16 @@ const Home = () => {
             removeCookie('token',{path:'/'});
         } else if(Auth.isLoggedIn(cookies.token)) {
             setDecodedToken(Auth.decodeToken(cookies.token))
-            console.log(Api.getPosts() + 'ooooo')
             axios({
                 method: 'get',
                 url: 'http://localhost:3080/api/posts/'
               }).then(res => {
-                console.log(res.data)
                 if(res.data.length === 0) return;
                 setPosts(res.data)
               }).catch(err => console.log(err))
         }
 
-    }, []);
+    }, [cookies.token, removeCookie]);
 
     const [postState, setPostState] = useState({ title: '', body: '' });
     // update state based on form input changes
@@ -147,7 +140,7 @@ const Home = () => {
 
                     </div>
                     <div data-id='posts' id={styles.midBlock} className='text-center'>
-                        {!posts ? 'no posts': 
+                        {!posts ? 'Feed empty': 
                             <div>
                                 {posts.map((post, index) => {
                                     return (
