e3d5f3d881cd750a978daede36b064a6586c1da0
Marshall Baccari <mbaccari@uw.edu>
2022-12-08 13:37:56 -0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Router>
+        <Router basename="/">
           <Routes>
             <Route
               path='/'
